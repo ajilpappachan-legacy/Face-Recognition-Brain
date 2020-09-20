778945714b0b134e7dd386b39e22c0c4046ecba9
ajilpappachan <ajilpappachan@gmail.com>
2020-09-21 00:11:54 +0530

    Created Database

--- Server/server.js
+++ Server/server.js
@@ -2,75 +2,122 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
-users = [
-    {
-        id: '123',
-        name: 'Ajil',
-        email: 'ajilpappachan@gmail.com',
-        password: 'jamesbond',
-        entries: 0,
-        joined: new Date()
+const database = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'smartbrain'
     }
-];
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.json(users);
+    database('users')
+    .returning('*')
+    .then(users => {
+        res.json(users);
+    })
+    
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === users[0].email && req.body.password === users[0].password) {
-        res.json(users[0]);
-    }
-    else {
-        res.json('Fail');
-    }
+    database.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid)
+        {
+            return database.select('*')
+            .from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => {
+                res.status(400).json('Unable to get user');
+            });
+        }
+        else {
+            res.status(400).json("wrong Credentials");
+        }
+    })
+    .catch(err => {
+        res.status(400).json("Wrong Credentials");
+    });
 })
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    users.push({
-        id: '124',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(users[users.length - 1]);
+    const hash = bcrypt.hashSync(password);
+    database.transaction(trx => {
+        trx
+            .insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        joined: new Date()
+                    })
+                    .then(user => res.json(user[0]))
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    })
+        .catch(err => {
+            res.status(400).json('Unspecified Error');
+        })
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    });
-    if (!found) {
-        res.json("No such user");
-    }
+    database.select('*')
+        .from('users')
+        .where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            }
+            else {
+                res.json('No Such User');
+            }
+        })
+        .catch(err => {
+            res.status(400).json('Unspecified Error');
+        });
 })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.json("No such user");
-    }
+    database('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            if (entries.length) {
+                res.json(entries[0]);
+            }
+            else {
+                res.json("No such Profile");
+            }
+        })
+        .catch(err => {
+            res.status(400).json("Unspecified Error");
+        })
 })
 
 app.listen(3000, () => {
